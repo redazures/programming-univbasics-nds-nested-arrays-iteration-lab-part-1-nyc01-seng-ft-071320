734fca5cf6c09d005a66d35aaa6387b1ed901271
james wu <jwu_nyc@yahoo.com>
2020-06-27 20:24:22 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ def find_even_values(src)
   # Output all even values in each nested array
   ora=[]
   src.each do |int|
-      ora<<int if num.even?
+      ora<<int if int.even?
     end
   ora
   end
